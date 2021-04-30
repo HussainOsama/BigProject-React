011eaa63a26cf4e0649d0b6a68e6bdd3d9ec1781
gunpowderman <adel.baroody@gmail.com>
2021-04-30 03:47:15 +0300

    live date data without salary cycle reset

--- stores/instance.js
+++ stores/instance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 //**** Code ****//
 
 const instance = axios.create({
-  baseURL: "http://192.168.100.232:8000",
+  baseURL: "http://192.168.10.139:8000",
 });
 
 export default instance;
